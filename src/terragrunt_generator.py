1ca7e94d93eb0583326e6ac03c98018b2e206592
Leonard Avevor <leonavevor@gmail.com>
2024-12-31 04:37:38 +0100

    wip, path not found fixed

--- src/terragrunt_generator.py
+++ src/terragrunt_generator.py
@@ -22,9 +22,9 @@ def generate_terragrunt_files(config):
             file.write(f"# Terragrunt configuration for {module['name']}\n")
             file.write(f"locals {{\n  # Add locals here\n}}\n")
             file.write(f'include "root" {{\n  path = find_in_parent_folders()\n}}\n')
-            file.write(f"dependency \"{module['depends_on']}\" {{\n # Add dependencies here\n}}\n")
+            #file.write(f"dependency \"{module['depends_on']}\" {{\n # Add dependencies here\n}}\n")
             if "git::" in module["source"]:
                 file.write(f"terraform {{\n  source = \"{module['source']}\"\n # Add additional configuration here\n}}\n")
             else:
-                file.write(f"terraform {{\n  source = \"${{get_repo_root()}}{module['source']}\"\n  # Add additional configuration here\n}}\n")
+                file.write(f"terraform {{\n  source = \"${{get_repo_root()}}/{module['source']}\"\n  # Add additional configuration here\n}}\n")
             file.write(f"inputs = {{\n # Add inputs here\n}}\n")
